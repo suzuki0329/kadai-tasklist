98ff5de8cb0cab14037e16253dec757d8117ce5a
sayuri.suzuki <endli0329@gmail.com>
2020-07-05 21:29:57 +0900

    Add database files

--- src/models/Task.java
+++ src/models/Task.java
@@ -25,6 +25,8 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -41,6 +43,14 @@ public class Task {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getContent() {
